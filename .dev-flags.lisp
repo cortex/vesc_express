92cb1f5d271af0afb601fdc60a6e2891b883ef29
Laxsjo <rasmus.soderhielm@gmail.com>
2023-07-19 12:50:31 +0200

    move dev flags to separate dot file

---
+++ .dev-flags.lisp
@@ -0,0 +1,22 @@
+;;; Dev flags (these disable certain features)
+
+(def dev-disable-low-battery-msg false)
+(def dev-disable-charging-msg false) ; does nothing right now...
+(def dev-short-thr-activation false)
+(def dev-disable-inactivity-check false) ; disables the check that deactivates the thrust upon 30 seconds of inactivity.
+(def dev-disable-connection-check false) ; disables the check that deactivates the thrust when connection has been lost.
+; (dev disable-sleep-button true)
+
+(def dev-force-view false) ; always show a specific view
+(def dev-view 'board-info) ; the view that will be shown
+(def dev-status-msg 'charging) ; only relevant when dev-view is 'status-msg
+(def dev-board-info-msg 'pairing) ; only relevant when dev-view is 'board-info
+
+(def dev-soc-remote nil) ; act as though the remote has the specified soc, nil to disable
+
+(def dev-bind-soc-bms-to-thr false) ; bind thrust input to bms soc meter. Usefull to test different values in a dynamic manner.
+(def dev-soc-bms-thr-ratio 0.25) ; thr-input is multiplied by this value before being assigned to the bms soc
+(def dev-bind-soc-remote-to-thr false) ; bind thrust input to the displayed remote soc. Usefull to test different values in a dynamic manner.
+(def dev-bind-speed-to-thr false) ; bind thrust input to the displayed remote soc. Usefull to test different values in a dynamic manner.
+
+(def dev-simulate-connection false)
