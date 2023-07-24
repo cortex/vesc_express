5ffdef5764ebd9dd37465e63e91be6a8b0e495b2
Laxsjo <rasmus.soderhielm@gmail.com>
2023-07-24 14:03:15 +0200

    make charge gradient height constant

--- main/include/views/view-charging.lisp
+++ main/include/views/view-charging.lisp
@@ -3,8 +3,11 @@
     (var icon (img-buffer-from-bin icon-large-battery))
     (sbuf-blit view-icon-buf icon 0 0 ())
     
-    (def view-bar-y 0)
-    (def view-gradient 0)
+    ; (def view-bar-y 0)
+    
+    (var col-secondary 0xdff3bd) ; TODO: Make proper palette entry for this color.
+    (def view-gradient (img-color 'gradient_y_pre col-accent col-secondary 115 0 'mirrored))
+    (gradient-calculate-easing view-gradient (weighted-ease ease-in-quint (construct-ease-out ease-in-quint) 0.2 0.8))
     
     (def view-status-text-buf (create-sbuf 'indexed2 25 240 140 24))
     (var text (img-buffer-from-bin text-charging))
@@ -19,23 +22,16 @@
         (sbuf-exec img-rectangle view-icon-buf 11 20 (62 115 0 '(filled)))
         
         (var height (to-i (* soc-remote 115.0)))
-        (def view-bar-y (+ 20 (- 115 height)))
-        (draw-rounded-rect view-icon-buf 11 view-bar-y 62 height 5 2)
-        
-        (var gradient-width height)
-        (var gradient-offset view-bar-y)
-        
-        (var col-secondary 0xdff3bd) ; TODO: Make proper palette entry for this color.
-        (def view-gradient (img-color 'gradient_y_pre col-accent col-secondary gradient-width gradient-offset 'mirrored))
-        (gradient-calculate-easing view-gradient (weighted-ease ease-in-quint (construct-ease-out ease-in-quint) 0.2 0.8))
+        (def y (+ 20 (- 115 height)))
+        (draw-rounded-rect view-icon-buf 11 y 62 height 5 2)
 
         (var percent-text (str-merge (str-from-n (to-i (* soc-remote 100.0))) "%"))
         (draw-text-centered view-charging-percent-buf 0 0 56 0 0 4 font-b1 1 0 percent-text)
     }))
     
-    (var height (to-i (* (state-get 'soc-remote) 115.0)))
+    ; (var height (to-i (* (state-get 'soc-remote) 115.0)))
         
-    (var total-secs 4.0)
+    (var total-secs 3.5)
     (var ease-secs 3.0)
     (var secs (secs-since view-timeline-start))
     (if (>= secs total-secs) {
@@ -48,9 +44,9 @@
         1.0
     ))
     
-    (var y-offset (to-i (* 2 height (- 1.0 ((weighted-smooth-ease ease-in-back ease-out-quint 0.6) anim-t)))))
+    (var y-offset (to-i (* 2 115 (- 1.0 ((weighted-smooth-ease ease-in-back ease-out-quint 0.6) anim-t)))))
     
-    (var offset (+ view-bar-y y-offset))
+    (var offset (+ 20 y-offset))
     (img-color-set view-gradient 'offset offset)    
 })
 
