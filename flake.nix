9afbca0e58816bc7494f9f600b31c65a75129cc4
Joakim Lundborg <joakim.lundborg@gmail.com>
2023-11-09 18:12:41 +0100

    Update Nix flake with correct version of esp-idf

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
-  outputs = { self, nixpkgs-esp-dev, nixpkgs, flake-utils }: 
+  outputs = { self, nixpkgs-esp-dev, nixpkgs}: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { 
@@ -13,7 +13,11 @@
     in {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [
-          pkgs.esp-idf-full
+          (pkgs.esp-idf-esp32c3.override {
+            rev = "v5.0.2";
+            sha256 = "sha256-dlmtTjoz4qQdFG229v9bIHKpYBzjM44fv+XhdDBu2Os=";
+          })
+          pkgs.python310Packages.future
         ];
       };
     };
