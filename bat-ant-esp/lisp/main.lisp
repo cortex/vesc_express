85ff62831fc1078d36356fcf009d6eeac6ab9813
Joakim Lundborg <joakim@lind.surf>
2024-03-20 13:10:57 +0100

    Remove dead code

--- bat-ant-esp/lisp/main.lisp
+++ bat-ant-esp/lisp/main.lisp
@@ -1,20 +1,7 @@
 (import "pkg@://vesc_packages/lib_code_server/code_server.vescpkg" 'code-server)
 (read-eval-program code-server)
 
-;(def remote-addr '(96   85 249 201 187 161)) ; Remote 1
-;(def remote-addr '(220  84 117 137  75  53)) ; Remote 2 (black)
-;(def remote-addr '(220  84 117  93  64  29)) ; Remote 3
-;(def remote-addr '(220  84 117 137 184 245)) ; Remote 4
-;(def remote-addr '(212 249 141   2 108  69)) ; Remote 6 (turqouise trigger)
-;(def remote-addr '(212 249 141   2 108 105)) ; Remote 7
-;(def remote-addr '(220  84 117 137 202 129)) ; Remote ?
-;(def remote-addr '( 52 133  24 192 211 181)) ; Remote ?
-
-
-;(def remote-addr '(84 50 4 135 207 237)) ; REV A SN05
-;(def remote-addr '(84 50 4 135 217 29)) ; REV A SN01
 (def remote-addr '(255 255 255 255 255 255)) ; Broadcast
-
 
 (esp-now-start)
 (esp-now-add-peer remote-addr)
