8cd04792d80d0126ba51e4f463c1fa5d518b6371
r3n33 <renee1382@gmail.com>
2024-03-26 12:19:30 -0500

    First pass boot animation v2

--- remote-disp-esp/lisp/main/include/views/boot-animation.lisp
+++ remote-disp-esp/lisp/main/include/views/boot-animation.lisp
@@ -1,86 +1,128 @@
-;;; Rising Sun Boot "Animation"
+;;; Boot Animation V2
+
+(defun compute-shades (time num-shades) {
+    (var initial-value 3.9) ; NOTE: Setting 3.9 for rounding to int while drawing
+    (var new-list nil)
+    (var i 0)
+    (var previous-value initial-value)
+    (loopwhile (< i num-shades) {
+        (var current-value (- previous-value (* (+ i initial-value) time)))
+        (setq previous-value current-value)
+        (setq current-value (if (< current-value 0) 0 current-value))
+        (setq new-list (append new-list (list current-value)))
+        (+set i 1)
+    })
+
+    (reverse new-list)
+})
 
 (defun boot-animation ()
 {
-    (var sun-start-y 180)
+    ; debugging: (var debug-first-compute true)
+
+    (var sun-start-y 140)
     (var sun-end-y 70)
     (var sun-height-offset sun-start-y)
     (var sun-gradient-shift 0)
 
-    (var vapor-0-thickness 1)
-    (var vapor-1-thickness 1)
-    (var vapor-2-thickness 2)
-    (var vapor-3-thickness 2)
-    (var vapor-4-thickness 3)
-    (var vapor-5-thickness 3)
-    (var vapor-start-y 120)
-    (var vapor-0-y vapor-start-y)
-    (var vapor-1-y (+ vapor-0-y 4))
-    (var vapor-2-y (+ vapor-1-y 4))
-    (var vapor-3-y (+ vapor-2-y 4))
-    (var vapor-4-y (+ vapor-3-y 4))
-    (var vapor-5-y (+ vapor-4-y 4))
-    (var vapor-1-start-y vapor-1-y)
-    (var vapor-2-start-y vapor-2-y)
-    (var vapor-3-start-y vapor-3-y)
-    (var vapor-4-start-y vapor-4-y)
-    (var vapor-5-start-y vapor-5-y)
-    (def rising-sun-buf (create-sbuf 'indexed2 50 59 141 142))
+    (var sun-lower-shade-start-y 75)
+    (var sun-lower-shade-y 75)
+
+    (var shade-count-sun 23)
+    (var shade-y-centers (list 3 9 15 21 27 33 39 45 51 57 63 69 75 81 87 93 99 105 111 117 123 129 135 141))
+    (var shade-heights (list 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3))
+
+    (var shade-count-logo 4)
+    (var logo-shade-y-centers (list 60 66 72 78))
+    (var logo-shade-heights (list 3 3 3 3))
+
+    (var rising-sun-buf (create-sbuf 'indexed2 50 59 141 142))
 
     (var logo (img-buffer-from-bin icon-lind-logo))
 
     (var start (systime))
     (var elapsed (secs-since start))
 
-    (var animation-time 2.5)
-    (if dev-fast-start-animation
-        (setq animation-time 1.0)
-    )
+    (var animation-time 3.0)
     (var animation-percent 0.0)
+    (var sun-rise-time 1.5)
+    (var sun-rise-percent 0.0)
 
     ; Watch Sunrise
     (var last-frame-time (systime))
-    (var fps-boot 0.0)
+    (def fps-boot 0.0)
     (loopwhile (< elapsed animation-time) {
         ; Update Animation Time
         (setq elapsed (secs-since start))
         (setq animation-percent (/ elapsed animation-time))
+        (if (< sun-rise-percent 1.0) {
+            (setq sun-rise-percent (/ elapsed sun-rise-time))
+            (if (> sun-rise-percent 1.0) (setq sun-rise-percent 1.0))
+        })
 
         ; Draw a sun in the buffer
         (sbuf-exec img-circle rising-sun-buf 70 sun-height-offset (70 1 '(filled)))
 
-        ; Draw vapor lines
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-0-y (142 vapor-0-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-1-y (142 vapor-1-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-2-y (142 vapor-2-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-3-y (142 vapor-3-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-4-y (142 vapor-4-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-5-y (142 vapor-5-thickness 0 '(filled)))
+        ; Draw the logo over the sun at the end of the animation
+        (if (> animation-percent 0.6) {
+            ; Draw logo on sun
+            (sbuf-blit rising-sun-buf logo 12 61 -1)
+            (sbuf-exec img-rectangle rising-sun-buf 0 69 (142 3 0 '(filled)))
+
+            ; Logo has 4 shades matching the color of the sun
+            (var i 0)
+            (loopwhile (< i 4) {
+                (sbuf-exec img-rectangle rising-sun-buf
+                    0
+                    (+ (ix logo-shade-y-centers i) (- 3.9 (ix logo-shade-heights i))) ; adjusting y for effect
+                    (142 (ix logo-shade-heights i) 1 '(filled)))
+                (+set i 1)
+            })
+
+            ; Adjust logo shades
+            (if (< animation-percent 0.75) {
+                (setq logo-shade-heights (compute-shades (ease-in-out-quart (map-range-01 animation-percent 0.6 0.75)) shade-count-logo))
+            })
+        })
+
+        ; Draw shades over the sun
+        (var i 0)
+        (loopwhile (< i 23) {
+            (sbuf-exec img-rectangle rising-sun-buf
+                0
+                (+ (ix shade-y-centers i) (- 3.9 (ix shade-heights i))) ; adjusting y for falling effect
+                (142 (ix shade-heights i) 0 '(filled)))
+            (+set i 1)
+        })
+
+        ; Reveal lower half of sun at the start of sun-rise-time
+        (if (< sun-rise-percent 0.75) {
+            (sbuf-exec img-rectangle rising-sun-buf 0 sun-lower-shade-y (142 100 0 '(filled)))
+            (setq sun-lower-shade-y (+ sun-lower-shade-start-y (* (ease-in-out-quart sun-rise-percent) 142)))
+        })
+
+        ; Close the blinds for the second half of the animation
+        (if (>= animation-percent 0.5) {
+            (setq shade-heights (compute-shades (ease-in-out-quart (* (- animation-percent 0.5) 2)) shade-count-sun))
+
+            ;(if debug-first-compute {
+            ;    (print shade-heights)
+            ;    (setq debug-first-compute false)
+            ;})
+        })
 
         ; Apply a gradient to the sun
         (sbuf-render rising-sun-buf (list
             0x000000
-            (img-color 'gradient_y 0xffa500 (lerp-color 0xff5347 0xffa500 (* animation-percent 0.9)) 142 0)
+            (img-color 'gradient_y 0xffa500 (lerp-color 0xbd1000 0xffa500 sun-rise-percent) 142 0)
+            ; debugging: 0xff0000
+            ; debugging: 0x00ff00
         ))
 
+        ; debugging: (if (> animation-percent 0.6) die)
+
         ; Sun height
-        (setq sun-height-offset (- sun-start-y (* (ease-in-out-sine animation-percent) (- sun-start-y sun-end-y))))
-
-        ; Vapor heights
-        (setq vapor-0-y (- vapor-start-y   (* (ease-in-out-sine animation-percent) (- vapor-start-y   70))))
-        (setq vapor-1-y (- vapor-1-start-y (* (ease-in-out-sine animation-percent) (- vapor-1-start-y 82))))
-        (setq vapor-2-y (- vapor-2-start-y (* (ease-in-out-sine animation-percent) (- vapor-2-start-y 94))))
-        (setq vapor-3-y (- vapor-3-start-y (* (ease-in-out-sine animation-percent) (- vapor-3-start-y 106))))
-        (setq vapor-4-y (- vapor-4-start-y (* (ease-in-out-sine animation-percent) (- vapor-4-start-y 118))))
-        (setq vapor-5-y (- vapor-5-start-y (* (ease-in-out-sine animation-percent) (- vapor-5-start-y 130))))
-
-        ; Vapor thickness
-        (setq vapor-0-thickness (to-i (- 1 (* (ease-in-out-sine animation-percent) (- 1 5)))))
-        (setq vapor-1-thickness (to-i (- 1 (* (ease-in-out-sine animation-percent) (- 1 5)))))
-        (setq vapor-2-thickness (to-i (- 2 (* (ease-in-out-sine animation-percent) (- 2 6)))))
-        (setq vapor-3-thickness (to-i (- 2 (* (ease-in-out-sine animation-percent) (- 2 7)))))
-        (setq vapor-4-thickness (to-i (- 3 (* (ease-in-out-sine animation-percent) (- 3 8)))))
-        (setq vapor-5-thickness (to-i (- 4 (* (ease-in-out-sine animation-percent) (- 3 9)))))
+        (setq sun-height-offset (- sun-start-y (* (ease-in-out-quart sun-rise-percent) (- sun-start-y sun-end-y))))
 
         ; Repeat
         (sbuf-clear rising-sun-buf)
@@ -90,54 +132,6 @@
         (setq last-frame-time (systime))
     })
     (print (str-merge "FPS Boot: " (to-str fps-boot)))
-
-    (setq start (systime))
-    (setq elapsed (secs-since start))
-
-    (setq animation-time 2.0)
-    (if dev-fast-start-animation
-        (setq animation-time 0.5)
-    )
-    (setq animation-percent 0.0)
-    ; Drop vapor lines before drawing logo
-    (loopwhile (< elapsed animation-time) {
-        (sbuf-clear rising-sun-buf)
-
-        ; Update Animation Time
-        (setq elapsed (secs-since start))
-        (setq animation-percent (/ elapsed animation-time))
-
-        ; Draw a sun in the buffer
-        (sbuf-exec img-circle rising-sun-buf 70 sun-height-offset (70 1'(filled)))
-
-        ; Draw Logo on Sun
-        (sbuf-blit rising-sun-buf logo 12 62 -1)
-
-        ; Draw vapor lines
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-0-y (142 vapor-0-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-1-y (142 vapor-1-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-2-y (142 vapor-2-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-3-y (142 vapor-3-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-4-y (142 vapor-4-thickness 0 '(filled)))
-        (sbuf-exec img-rectangle rising-sun-buf 0 vapor-5-y (142 vapor-5-thickness 0 '(filled)))
-
-        ; Vapor heights
-        (setq vapor-1-y (- 82 (* (ease-in-out-sine animation-percent) (- 82 200))))
-        (setq vapor-2-y (- 94 (* (ease-in-out-sine animation-percent) (- 94 200))))
-        (setq vapor-3-y (- 106 (* (ease-in-out-sine animation-percent) (- 106 200))))
-        (setq vapor-4-y (- 118 (* (ease-in-out-sine animation-percent) (- 118 200))))
-        (setq vapor-5-y (- 130 (* (ease-in-out-sine animation-percent) (- 130 200))))
-
-        ; Vapor thickness
-        (setq vapor-1-thickness (to-i (- 5 (* (ease-in-out-sine animation-percent) (- 5 10)))))
-        (setq vapor-2-thickness (to-i (- 6 (* (ease-in-out-sine animation-percent) (- 6 12)))))
-
-        ; Render buffer
-        (sbuf-render rising-sun-buf (list
-            0x000000
-            0xffa500
-        ))
-    })
 
     ; Draw version number
     (var w (* (bufget-u8 font-b3 0) (str-len version-str)))
