4def70b6086dd220108ba6746fa0a193c52e645c
r3n33 <renee1382@gmail.com>
2024-05-10 14:28:47 -0500

    Sending FW Install Ready to API with status check

--- bat-bms-esp/lisp/status.lisp
+++ bat-bms-esp/lisp/status.lisp
@@ -1,3 +1,5 @@
+@const-start
+
 (def api-url "http://lindboard-staging.azurewebsites.net/api/esp")
 
 ;JSON helpers
@@ -30,16 +32,16 @@
                     (kv "longitude"              (str-from-n 18.0686)) "," ; TODO: what to do in case of error
                     (kv "celcius"                (int (get-bms-val 'bms-temp-hum)))
                 "}"
-                ;",{" (kv "serialNumber" (q serial-number-remote)) "}"
-                ;",{" (kv "serialNumber" (q serial-number-jet)) "}"
-                ;",{" (kv "serialNumber" (q serial-number-board)) "}"
+            ;",{" (kv "hardwareIdentifier" (q serial-number-remote)) "," (kv "serialNumber" (q serial-number-remote)) "," (kv "chargeLevel" (int 42)) "," (kv "chargeStatus" (int 2)) "}"
+            ;",{" (kv "hardwareIdentifier" (q serial-number-jet))    "," (kv "serialNumber" (q serial-number-jet))    "," (kv "chargeStatus" (int 1)) "}"
+            ;",{" (kv "hardwareIdentifier" (q serial-number-board))  "," (kv "serialNumber" (q serial-number-board))  "," (kv "chargeStatus" (int 1)) "}"
             "]}"
         )
     )
 ))
 
-(define registration-id nil)
-(define registration-id "dab20f85-4ea7-4b70-bb02-848f0e82f8db")
+(define registration-id nil) ; TODO: This should be stored somewhere
+(define registration-id "69c1446a-2b16-4449-8a56-9a97a17e1736")
 
 (defunret send-status (){
     (var url (str-merge api-url "/batteryStatusUpdate"))
@@ -55,9 +57,55 @@
             (var req (http-post-json url status-json))
             (var res (tcp-send conn req))
             (var response (http-parse-response conn))
-            (var result (ix (ix response 0) 1))
+            (var result (second (first response)))
+            (if (eq "200" result) {
+                ; Read body for hardwareActionId processing
+                (var content-length (http-parse-content-length response))
+                (if (not-eq content-length nil) {
+                    (var resp-body (tcp-recv conn content-length))
+                    ;(print resp-body)
+                    (def hw-actions (parse-json-firmware resp-body))
+                    (var i 0)
+                    (loopwhile (< i (length hw-actions)) {
+                        ; TODO: Perform hardware actions, such as initiating fw install
+                        (print (second (ix hw-actions i)))
+                        (setq i (+ i 1))
+                    })
+                })
+            })
             (tcp-close conn)
             (if (eq "200" result) 'ok 'error)
+        })
+})
+
+(defun fw-ready-json () (cond
+    ((not registration-id) 'no-registration-id)
+    (t (str-merge
+            "{" (kv "registrationId" (q registration-id)) ", "
+                ((kv "hardwareIdentifier" (q (str-merge "BA" (mac-addr-string)))) ","
+                (kv "firmwareId" (int fw-id-board-downloaded))
+            "}"
+        )
+    )
+))
+
+(defunret send-fw-ready (){
+    (var url (str-merge api-url "/readyToInstallFirmware"))
+    (var conn (tcp-connect (url-host url) (url-port url)))
+    (if (or (eq conn nil) (eq conn 'unknown-host))
+        (print (str-merge "error connecting to " (url-host url) " " (to-str conn))) 
+        {
+            (var status-json (fw-ready-json))
+            (if (not (eq (type-of status-json) 'type-array)) {
+                (tcp-close conn)
+                (return status-json)
+            })
+            (var req (http-post-json url status-json))
+            (var res (tcp-send conn req))
+            (var response (http-parse-response conn))
+            (var result (second (first response)))
+            (tcp-close conn)
+            (if (eq "204" result) 'ok 'error)
         })
 })
 
@@ -69,6 +117,8 @@
 
 (defun status-loop () {
         (print (str-merge "Status ping: " (to-str (send-status))))
+        (gc)
+        (if fw-install-ready (print (str-merge "FW ready ping: " (to-str (send-fw-ready)))))
         (gc)
         (sleep 5)
         (status-loop)
