726e2176b934099c3797da676dd24a6ffb849b58
r3n33 <renee1382@gmail.com>
2024-05-10 13:32:18 -0500

    Adding example update-description files

---
+++ bat-ant-esp/lisp/update-descriptions/update-description-all-devices.lisp
@@ -0,0 +1,19 @@
+(def update-description (list
+    '(jet-if-esp . fw-vesc)
+
+    '(bat-ant-stm . fw-vesc)
+
+    '(bat-bms-stm . fw-vesc)
+
+    '(bat-bms-esp . fw-vesc)
+    '(bat-bms-esp . fw-lisp)
+
+    '(bat-esc-stm . fw-vesc)
+    '(bat-esc-stm . fw-lisp)
+
+    '(remote-disp-esp . fw-vesc-espnow)
+    '(remote-disp-esp . fw-lisp-espnow)
+
+    '(bat-ant-esp . fw-vesc)
+    '(bat-ant-esp . fw-lisp)
+))
---
+++ bat-ant-esp/lisp/update-descriptions/update-description-remote.lisp
@@ -0,0 +1,4 @@
+(def update-description (list
+    '(remote-disp-esp . fw-vesc-espnow)
+    '(remote-disp-esp . fw-lisp-espnow)
+))
