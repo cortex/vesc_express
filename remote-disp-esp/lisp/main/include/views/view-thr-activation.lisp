4bca4015613a34546af0427c5a582417c16153a3
r3n33 <renee1382@gmail.com>
2024-05-21 15:39:05 -0500

    Update throttle activation view

--- remote-disp-esp/lisp/main/include/views/view-thr-activation.lisp
+++ remote-disp-esp/lisp/main/include/views/view-thr-activation.lisp
@@ -9,36 +9,34 @@
 
 (defun view-init-thr-activation () {
     ; large center graphic
-    (def view-graphic-buf (create-sbuf 'indexed4 (- 120 90) 46 181 182))
-    (def angle-previous 90.0)
+    (def view-graphic-buf (create-sbuf 'indexed4 (- 120 70) (+ 50 display-y-offset) 141 142))
+    (def view-angle-previous 90.0)
 
     ; status text
-    (def view-status-text-buf (create-sbuf 'indexed4 (- 120 90) 230 180 76))
+    (def view-status-text-buf (create-sbuf 'indexed4 (- 120 90) (+ 220 display-y-offset) 180 26))
 })
 
 (defun view-draw-thr-activation () {
     (state-with-changed '(thr-activation-state) (fn (thr-activation-state) {
         (sbuf-clear view-graphic-buf)
-        (sbuf-exec img-circle view-graphic-buf 90 90 (70 2 '(filled)))
+        (sbuf-exec img-circle view-graphic-buf 70 70 (70 2 '(filled)))
 
-        (if (or
-            (eq thr-activation-state 'release-warning)
-            (eq thr-activation-state 'countdown)
-        ) {
+        (if (eq thr-activation-state 'release-warning)
+        {
             ; White Circle
-            (sbuf-exec img-circle view-graphic-buf 90 90 (35 3 '(thickness 6)))
+            (sbuf-exec img-circle view-graphic-buf 70 70 (35 3 '(thickness 6)))
             ; Exclamation
-            (sbuf-exec img-rectangle view-graphic-buf (- 90 3) 74 (6 20 3 '(filled)))
-            (sbuf-exec img-rectangle view-graphic-buf (- 90 3) 101 (6 6 3 '(filled)))
-            (setq angle-previous 90.0)
+            (sbuf-exec img-rectangle view-graphic-buf (- 70 3) 54 (6 20 3 '(filled)))
+            (sbuf-exec img-rectangle view-graphic-buf (- 70 3) 81 (6 6 3 '(filled)))
+            (setq view-angle-previous 90.0)
         } {
             ; three empty circles
-            (sbuf-exec img-circle view-graphic-buf 91 52 (20 3 '(thickness 2)))
-            (sbuf-exec img-circle view-graphic-buf 51 91 (20 3 '(thickness 2)))
-            (sbuf-exec img-circle view-graphic-buf 129 91 (20 3 '(thickness 2)))
+            (sbuf-exec img-circle view-graphic-buf 70 35 (18 3 '(thickness 2)))
+            (sbuf-exec img-circle view-graphic-buf 34 70 (18 3 '(thickness 2)))
+            (sbuf-exec img-circle view-graphic-buf 104 70 (18 3 '(thickness 2)))
             ; one full circle
-            (sbuf-exec img-circle view-graphic-buf 91 129 (20 1 '(filled)))
-            (setq angle-previous 90.0)
+            (sbuf-exec img-circle view-graphic-buf 70 106 (20 1 '(filled)))
+            (setq view-angle-previous 90.0)
         })
         
         (sbuf-clear view-status-text-buf)
@@ -57,18 +55,18 @@
         (var value (/ secs thr-countdown-len-secs))
         (var angle (+ 90 (* value 360)))
         
-        (sbuf-exec img-arc view-graphic-buf 90 90 (90 angle-previous angle 1 '(thickness 17)))
-        (setq angle-previous angle)
+        (sbuf-exec img-arc view-graphic-buf 70 70 (70 view-angle-previous angle 1 '(thickness 4)))
+        (setq view-angle-previous angle)
     })
 })
 
 (defun view-render-thr-activation () {
-    (sbuf-render-changes view-graphic-buf (list 0x0 0x7f9a0d 0x262626 0xffffff))
+    (sbuf-render-changes view-graphic-buf (list col-black 0x7f9a0d 0x353535 col-white))
     (sbuf-render-changes view-status-text-buf (list col-black col-text-aa1 col-text-aa2 col-white))
 })
 
 (defun view-cleanup-thr-activation () {
     (def view-graphic-buf nil)
-    (def angle-previous nil)
+    (def view-angle-previous nil)
     (def view-status-text-buf nil)
 })
