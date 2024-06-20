9886d23c97e40dafcb09bbd6969126fad04c48e9
Joakim Lundborg <joakim@lind.surf>
2024-06-20 17:05:01 +0200

    Bump vesc tool

--- vesc-tool.nix
+++ vesc-tool.nix
@@ -13,7 +13,7 @@ pkgs.stdenv.mkDerivation rec {
     owner = "vedderb";
     repo = "vesc_tool";
     rev = "master";
-    hash = "sha256-sF+rMKnroPvGlsZ06aUQi7wf2MM5ew0uZ6/A6Ef5LEk=";
+    hash = "sha256-Xfvf9Chq79O7eVe3qPow0Svwkv/whBPZaKU3crETSRw=";
   };
   
   patches = [
