9e5139ed28036ccb34358018c13d6d721a404d1a
Joakim Lundborg <joakim@lind.surf>
2024-07-12 14:11:05 +0200

    Add openloop test script

---
+++ bat-esc-stm/lisp/scripts/openloop-test.lisp
@@ -0,0 +1,16 @@
+; Set sensor mode to Sensorless
+(conf-set 'foc-sensor-mode 0)
+
+; Run resistanse measurement
+; Should be between 2-3 mOhm
+(conf-measure-res 100)
+
+
+; Run the openloop test
+(foc-openloop 100 100)
+;(foc-openloop 200 100)
+;(foc-openloop 290 100)
+
+
+; Reset sensor mode
+(conf-set 'foc-sensor-mode 0)
